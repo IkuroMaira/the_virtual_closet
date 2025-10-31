579974516a422e9433f01b660ff629c883daa1a4
IkuroMaira <gwen.bussac@protonmail.com>
2025-10-31 14:59:38 +0100

    Update connexion system

---
+++ app/db/database.py
@@ -0,0 +1,21 @@
+from dotenv import load_dotenv
+import os
+from supabase import create_client, Client
+
+load_dotenv()
+
+SUPABASE_URL= os.getenv('SUPABASE_URL')
+SUPABASE_KEY= os.getenv('SUPABASE_KEY')
+
+def get_supabase() -> Client:
+    """
+    Fonction de dépendance pour FastAPI
+    Retourne un client Supabase pour chaque requête
+    """
+    return create_client(SUPABASE_URL, SUPABASE_KEY)
+
+# Test de connexion (optionnel, tu peux le supprimer après)
+# if __name__ == "__main__":
+#     supabase = get_supabase()
+#     results = supabase.table('clothes').select('*').execute()
+#     print(results)
--- db/database.py
+++
@@ -1,9 +0,0 @@
-from supabase import create_client, Client
-
-SUPABASE_URL=''
-SUPABASE_KEY=''
-
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
-
-results = supabase.table('clothes').select('*').execute()
-print(results)
