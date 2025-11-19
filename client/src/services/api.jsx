959b2fb2b94e7eec94cf4fe2e6c26745c47d6e84
IkuroMaira <gwen.bussac@protonmail.com>
2025-11-19 15:21:56 +0100

    Display clothe categories in a select

--- client/src/services/api.jsx
+++ client/src/services/api.jsx
@@ -27,26 +27,3 @@ export async function testConnection() {
     throw error
   }
 }
-
-export default async function Clothes() {
-  console.log("=> Appel : Clothes")
-
-  try {
-    console.log('=> Envoi de la requête vers', API_BASE_URL + '/clothes')
-    const response = await fetch(API_BASE_URL + '/clothes')
-
-    if (!response.ok) {
-      throw new Error(`Erreur HTTP! Status: ${response.status}`)
-    }
-
-    console.log('=> Réponse reçue:', response)
-
-    const clothesData = await response.json()
-    console.log('=> Données:', clothesData)
-
-    return clothesData
-  } catch (error) {
-    console.error('=> Erreur dans testConnection:', error)
-    throw error
-  }
-}
