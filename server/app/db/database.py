43a4f9d22bd06ee7db21909e9138bebdad410512
Majda-Fg <majda.fougou@gmail.com>
2025-11-28 17:59:24 +0100

    adding repository folder & insert tag into database handled by the ORM

--- server/app/db/database.py
+++ server/app/db/database.py
@@ -1,21 +1,32 @@
 from dotenv import load_dotenv
+""" from supabase import create_client, Client """
 import os
-from supabase import create_client, Client
+from sqlmodel import create_engine, Session, SQLModel
 
 load_dotenv()
+
+""" load_dotenv()
 
 SUPABASE_URL= os.getenv('SUPABASE_URL')
 SUPABASE_KEY= os.getenv('SUPABASE_KEY')
 
 def get_supabase() -> Client:
-    """
+    
     Fonction de dépendance pour FastAPI
     Retourne un Client Supabase pour chaque requête
-    """
+    
     return create_client(SUPABASE_URL, SUPABASE_KEY)
-
+ """
 # Test de connexion (optionnel, tu peux le supprimer après)
 # if __name__ == "__main__":
 #     supabase = get_supabase()
 #     results = supabase.table('clothes').select('*').execute()
 #     print(results)
+
+DATABASE_URL = os.getenv('DATABASE_URL')
+
+engine = create_engine(DATABASE_URL, echo=True)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
--- server/app/routers/tags_router.py
+++ server/app/routers/tags_router.py
@@ -2,34 +2,35 @@ import logging
 
 from fastapi import APIRouter, Depends, HTTPException
 from supabase import Client
-from app.db.database import get_supabase
-from app.services import tags_service
+from app.db.database import get_session
+from app.repository import tags_repository
 from app.models import tags
+from sqlmodel import Session
 
 router = APIRouter(
     prefix="/tags",
     tags=["Tags"]
 )
 
-@router.post("/new_tag")
-def create_tag(tag: tags.Tag, supabase: Client = Depends(get_supabase)):
+@router.post("/new_tag", response_model=tags.TagPublic)
+def create_tag(tag: tags.TagCreate, session: Session = Depends(get_session)):
     """ 
     Create a new tag
     """
     try:
-        new_tag = tags_service.create_tag(tag, supabase)
-        return {"item": new_tag}
+        new_tag = tags_repository.create_tag(tag, session)
+        return new_tag
     except Exception as e:
-        raise HTTPException(status_code=400, detail="Impossible d'insérer le tag")
+        raise HTTPException(status_code=400, detail=str(e))
     
 
-@router.get("/")
+""" @router.get("/")
 def get_all_tags(supabase: Client = Depends(get_supabase)):
-    """
+    
     Get all tags 
-    """
+    
     try:
-        tags = tags_service.get_all_tags(supabase)
+        tags = tags_repository.get_all_tags(supabase)
 
         if tags is None:
             logging.error("Les tags n'existent pas")
@@ -41,11 +42,11 @@ def get_all_tags(supabase: Client = Depends(get_supabase)):
 
 @router.get("/{item_id}")
 def get_tag(tag_id: int, supabase: Client = Depends(get_supabase)):
-    """
+    
     Get a tag
-    """
+    
     try:
-        tag = tags_service.get_tag(supabase, tag_id)
+        tag = tags_repository.get_tag(supabase, tag_id)
 
         if tag is None:
             logging.error("Le tag n'existe pas")
@@ -57,11 +58,11 @@ def get_tag(tag_id: int, supabase: Client = Depends(get_supabase)):
 
 @router.put("/{item_id}/update")
 def update_tag(tag_id: int, tag: tags.Tag, supabase: Client = Depends(get_supabase)):
-    """
+    
     Update a tag
-    """
+    
     try:
-        updated_tag = tags_service.update_tag(tag, supabase, tag_id)
+        updated_tag = tags_repository.update_tag(tag, supabase, tag_id)
 
         if updated_tag is None:
             logging.error("Le tag n'existe pas")
@@ -73,15 +74,15 @@ def update_tag(tag_id: int, tag: tags.Tag, supabase: Client = Depends(get_supaba
 
 @router.delete("/{item_id}/delete")
 def delete_tag(tag_id: int, supabase: Client = Depends(get_supabase)):
-    """
+    
     Delete a piece of clothe
-    """
+    
     try:
-        deleted_tag = tags_service.delete_tag(supabase, tag_id)
+        deleted_tag = tags_repository.delete_tag(supabase, tag_id)
 
         if deleted_tag is None:
             logging.error("Le tag n'existe pas")
 
         return  {"deleted_tag": deleted_tag}
     except Exception:
-        raise HTTPException(status_code=500, detail="Impossible de supprimer le tag")
+        raise HTTPException(status_code=500, detail="Impossible de supprimer le tag") """
