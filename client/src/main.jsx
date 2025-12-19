4353324ac924ec39be935032fd8fee9e99f8dc3a
IkuroMaira <gwen.bussac@protonmail.com>
2025-12-19 16:40:09 +0100

    WIP

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider, createRouter } from '@tanstack/react-router'
 import './index.css'
-import App from './App.jsx'
+
+// Import du "route tree" généré automatiquement par le plugin
+import { routeTree } from './routeTree.gen'
+
+// Création du routeur avec l'arbre de routes
+const router = createRouter({ routeTree })
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
