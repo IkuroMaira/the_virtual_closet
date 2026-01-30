6a43d0de772a6c592c83886ce72582ae8b17d62e
Majda-Fg <149064079+Majda-Fg@users.noreply.github.com>
2026-01-30 19:56:11 +0100

    adding repository folder & insert tag into database handled by the ORM (#66)
    * adding repository folder & insert tag into database handled by the ORM
    
    * orm for tag CRUD's completed
    
    * adding orm to clothes table + code cleanup

--- server/app/models/tags.py
+++ server/app/models/tags.py
@@ -1,11 +1,39 @@
-from pydantic import BaseModel, Field
+from sqlmodel import Field, SQLModel
+from datetime import datetime 
 
 # ============================================
 # PYDANTIC SCHEMAS (Models)
 # ============================================
 
-class Tag(BaseModel):
-    id: int = Field(...)
-    name: str = Field(min_length=2,max_length=20)
-    color: str
-    
+class TagCreate(SQLModel):
+    name: str
+    color: str 
+    created_at : datetime = Field(default_factory=datetime.now)
+    by_default : bool = True
+    user_id : int | None = None
+
+
+""" TO DO : réfléchir champ obligatoire ou optionnel """
+class Tags(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str
+    color: str 
+    created_at : datetime = Field(default_factory=datetime.now)
+    by_default : bool = True
+    user_id : int | None = Field(default=None, foreign_key="users.id")
+
+
+class TagPublic(SQLModel):
+    id: int 
+    name: str
+    color: str 
+    created_at : datetime 
+    by_default : bool 
+    user_id : int | None 
+
+
+class TagUpdate(SQLModel):
+    name: str | None = None
+    color: str | None = None
+    by_default : bool | None = None
+    user_id : int | None = None
