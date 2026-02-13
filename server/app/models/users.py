81e091d124ccc0645d5bd748186a26d49130b4ff
IkuroMaira <gwen.bussac@protonmail.com>
2026-02-13 21:11:14 +0100

    Add fields to Users and Brands models

--- server/app/models/users.py
+++ server/app/models/users.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from sqlmodel import Field, SQLModel
 
 # ============================================
@@ -7,3 +9,8 @@ from sqlmodel import Field, SQLModel
 
 class Users(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
+    pseudo : str
+    password: str
+    email: str
+    created_at: datetime = Field(default_factory=datetime.now)
+    updated_at: datetime = Field(default_factory=datetime.now)
