797078766370b4785f2021a28c80a09b0c4a2503
Majda-Fg <majda.fougou@gmail.com>
2026-03-01 23:01:53 +0100

    cleaning tags_clothes router and repository

--- server/app/repository/tags_clothes_repository.py
+++ server/app/repository/tags_clothes_repository.py
@@ -7,117 +7,116 @@ from sqlalchemy import func
 
 logger = logging.getLogger(__name__)
 
-def get_all_tags_from_item(session: Session, item_id: int) -> list[Tags]:
+def get_all_tags_from_item(item_id: int, session: Session) -> list[Tags]:
     """     
-    Get all tags for a piece of clothe
+    Get all tags for a piece of clothing
     
     Args:
-        session (Session): SQLModel session connected to the database
         item_id (int): ID of the clothing item
+        session (Session): SQLModel session connected to the database
 
     Returns:
-        List: Tags object assigned to a piece of clothe
-
+        List: Tags object assigned to a piece of clothing (empty list if no tags)
+    
     Raises: 
-        Exception: If error during retrieval
+        ValueError: If item doesn't exist
     """
-    try:
-        clothe = session.get(Clothes, item_id)
-        if not clothe:
-            raise ValueError(f"Le vêtement avec l'ID {item_id} n'existe pas")
-        statement = (select(Tags).join(Tags_Clothes, Tags.id == Tags_Clothes.tag_id).where(Tags_Clothes.clothe_id == item_id))
-        tags = session.exec(statement).all()
-        return tags
-
-    except Exception as e:
-        logger.error(f"Erreur lors de la récupération des tags du vêtement: {str(e)}")
-        raise Exception("Erreur lors de la récupération des tags du vêtement")
+    clothe = session.get(Clothes, item_id)
+    if not clothe:
+        raise ValueError(f"Le vêtement avec l'ID {item_id} n'existe pas")
+    statement = (
+        select(Tags)
+        .join(Tags_Clothes, Tags.id == Tags_Clothes.tag_id)
+        .where(Tags_Clothes.clothe_id == item_id))
+    tags = session.exec(statement).all()
+    return tags
     
 
-def add_tag_to_item(item_id: int, session: Session, tag_id: int, user_id:int) -> Tags_Clothes:
+def add_tag_to_item(item_id: int, tag_id: int, user_id:int, session: Session) -> Tags_Clothes:
     """ 
     Assign a tag to an item
 
     Args:
+        item_id (int): ID of the clothing item
+        tag_id: ID of the tag
+        user_id: Id of the user (user_id:2 by default)
+        session (Session): SQLModel session connected to the database
 
     Returns:
+        Tag_Clothes: the association between a tag and a piece of clothing
 
     Raises:
-
+        ValueError: If item/tag doesn't exist, or if an association exists already
     """
-    try:
-        clothe = session.get(Clothes, item_id)
-        if not clothe:
-            raise ValueError(f"Le vêtement avec l'ID {item_id} n'existe pas")
+    clothe = session.get(Clothes, item_id)
+    if not clothe:
+        raise ValueError(f"Le vêtement avec l'ID {item_id} n'existe pas")
         
-        tag = session.get(Tags, tag_id)
-        if not tag:
-            raise ValueError(f"Le tag avec l'ID {tag_id} n'existe pas")
+    tag = session.get(Tags, tag_id)
+    if not tag:
+        raise ValueError(f"Le tag avec l'ID {tag_id} n'existe pas")
+    
+    existing_relation = session.exec(
+        select(Tags_Clothes)
+        .where(Tags_Clothes.clothe_id == item_id)
+        .where(Tags_Clothes.tag_id == tag_id)
+    ).first()
+    
+    if existing_relation:
+        raise ValueError(f"Le tag est déjà assigné à ce vêtement")
         
-        existing_relation = session.exec(
-            select(Tags_Clothes)
-            .where(Tags_Clothes.clothe_id == item_id)
-            .where(Tags_Clothes.tag_id == tag_id)
-        ).first()
+    current_tags_count = session.exec(
+        select(func.count(Tags_Clothes.id))
+        .where(Tags_Clothes.clothe_id == item_id)
+    ).one()
     
-        if existing_relation:
-            raise ValueError(f"Le tag est déjà assigné à ce vêtement")
-        
-        current_tags_count = session.exec(
-            select(func.count(Tags_Clothes.id))
-            .where(Tags_Clothes.clothe_id == item_id)
-        ).one()
+    if current_tags_count >= 10:
+        raise ValueError(f"Le vêtement a déjà 10 tags (maximum atteint)")
     
-        if current_tags_count >= 10:
-            raise ValueError(f"Le vêtement a déjà 10 tags (maximum atteint)")
-    
-        new_relation = Tags_Clothes(
-            clothe_id=item_id,
-            tag_id=tag_id,
-            user_id=user_id  #  gérer dans l'Epic 3
-        )
+    new_relation = Tags_Clothes(
+        clothe_id=item_id,
+        tag_id=tag_id,
+        user_id=user_id  # à gérer dans l'Epic 3
+    )
             
-        session.add(new_relation)
-        session.commit()
-        session.refresh(new_relation)
+    session.add(new_relation)
+    session.commit()
+    session.refresh(new_relation)
     
-        return new_relation
-    except Exception as e:
-        logger.error(f"Erreur lors de l'assignation du tag au vêtement: {str(e)}")
-        raise Exception("Erreur lors de l'assignation du tag au vêtement:")
+    return new_relation
     
 
-def remove_tag_from_item(session: Session, item_id: int, tag_id: int) -> Tags_Clothes:
+def remove_tag_from_item(item_id: int, tag_id: int, session: Session) -> Tags_Clothes:
     """ 
     Delete a tag assigned to an item
      
     Args:
-    
+        item_id (int): ID of the clothing item
+        tag_id: Id of the tag
+        session (Session): SQLModel session connected to the database
      
     Returns:
-     
-    Raises: 
+        Tag_Clothes: the deleted relation between a tag and a piece of clothing
+
+    Raises:
+        ValueError: If relation doesn't exist
     """
-    try:
-        relation = session.exec(
-            select(Tags_Clothes)
-            .where(Tags_Clothes.clothe_id == item_id)
-            .where(Tags_Clothes.tag_id == tag_id)
-        ).first()
+    relation = session.exec(
+        select(Tags_Clothes)
+        .where(Tags_Clothes.clothe_id == item_id)
+        .where(Tags_Clothes.tag_id == tag_id)
+    ).first()
 
-        if not relation:
-            raise ValueError(f"Le tag {tag_id} n'est pas assigné au vêtement {item_id}")
+    if not relation:
+        raise ValueError(f"Le tag {tag_id} n'est pas assigné au vêtement {item_id}")
 
-        deleted_relation = Tags_Clothes(
-            id=relation.id,
-            clothe_id=relation.clothe_id,
-            tag_id=relation.tag_id,
-            user_id=relation.user_id
+    deleted_relation = Tags_Clothes(
+        id=relation.id,
+        clothe_id=relation.clothe_id,
+        tag_id=relation.tag_id,
+        user_id=relation.user_id
     )    
-        session.delete(relation)
-        session.commit()
+    session.delete(relation)
+    session.commit()
 
-        return deleted_relation
-    except Exception as e:
-        logger.error(f"Erreur lors de la suppression de l'association du tag au vêtement: {str(e)}")
-        raise Exception("Erreur lors de la suppression de l'association du tag au vêtement:")
+    return deleted_relation
--- server/app/routers/tags_clothes_router.py
+++ server/app/routers/tags_clothes_router.py
@@ -14,38 +14,65 @@ router = APIRouter(
 @router.get("/{item_id}/tags", response_model=list[TagPublic])
 def get_all_tags_from_item(item_id: int, session: Session = Depends(get_session)):
     """
-    Get all tags assigned to a specific piece of clothe
+    Get all tags assigned to a specific piece of clothing
     """
     try:
-        clothe_tags = tags_clothes_repository.get_all_tags_from_item(session, item_id)
+        clothe_tags = tags_clothes_repository.get_all_tags_from_item(item_id, session)
         return clothe_tags
-    except Exception as e:
-        raise HTTPException(status_code=404, detail=str(e))
+    
+    except ValueError as e:
+        error_msg = str(e)
+        if "n'existe pas" in str(e):
+            raise HTTPException(status_code=404, detail=error_msg)
+        else:
+            raise HTTPException(400, detail=error_msg)
+        
+    except Exception:
+        logging.error(f"Erreur technique lors de la récupération des tags du vêtement {item_id}")
+        raise HTTPException(status_code=500, detail="Erreur interne lors de la récupèreration des tags de ce vêtement")
 
 
 @router.post("/{clothe_id}/tags/{tag_id}", response_model=Tags_Clothes)
 def add_tag_to_item(item_id: int, tag_id: int, user_id: int = 2, session: Session = Depends(get_session)):
     """      
-    Create a tag association with a piece of clothe
+    Create a tag association with a piece of clothing
     """
     try:
-        tag_association = tags_clothes_repository.add_tag_to_item(item_id, session, tag_id, user_id)
+        tag_association = tags_clothes_repository.add_tag_to_item(item_id, tag_id, user_id, session)
         return tag_association
-    except Exception as e:
-        raise HTTPException(status_code=404, detail="Impossible de créer une association entre le tag et le vêtement")
+    
+    except ValueError as e:
+        error_msg = str(e)
+        if "n'existe pas" in error_msg:
+            raise HTTPException(status_code=404, detail=error_msg)
+        elif "déjà assigné" in error_msg:
+            raise HTTPException(status_code=409, detail=error_msg)
+        elif "maximum" in error_msg:
+            raise HTTPException(status_code=400, detail=error_msg)
+        else:
+            raise HTTPException(400, detail=error_msg)
+        
+    except Exception:  
+        logging.error(f"Erreur technique lors de l'ajout du tag {tag_id} au vêtement {item_id}")
+        raise HTTPException(status_code=500, detail="Erreur interne lors de l'ajout du tag à un vêtement")
 
 
 @router.delete("/{clothe_id}/tags/{tag_id}", response_model=Tags_Clothes)
 def remove_tag_from_item(item_id: int, tag_id: int, session: Session = Depends(get_session)):
     """          
-    Delete a tag association with a piece of clothe
+    Delete a tag association with a piece of clothing
     """
     try:
-        deleted_tag_item = tags_clothes_repository.remove_tag_from_item(session, item_id, tag_id)
-
-        if deleted_tag_item is None:
-            logging.error("L'association entre le tag et le vêtement n'existe pas")
-
+        deleted_tag_item = tags_clothes_repository.remove_tag_from_item(item_id, tag_id, session)
         return deleted_tag_item
+    
+    except ValueError as e:
+        error_msg = str(e)
+        if "n'est pas assigné" in error_msg:
+            raise HTTPException(status_code=404, detail=error_msg)
+        else:
+            raise HTTPException(400, detail=error_msg)
+        
     except Exception:
-        raise HTTPException(status_code=500, detail="Impossible de supprimer l'association entre le vêtement et le tag")
+        logging.error(f"Erreur technique lors du retrait du tag {tag_id} au vêtement {item_id}")
+        raise HTTPException(status_code=500, detail="Erreur interne lors du retrait du tag au vêtement")
