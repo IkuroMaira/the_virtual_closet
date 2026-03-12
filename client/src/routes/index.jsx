bc96397f8196755b3277cda7b72f32497b818a07
IkuroMaira <gwen.bussac@protonmail.com>
2026-03-12 18:33:11 +0100

    Remove test connection button

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -1,6 +1,4 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { useState } from 'react'
-import { testConnection } from '../shared/services/api.jsx'
 import ClothesDisplay from '../features/wardrobe/components/ClothesDisplay.jsx'
 
 // index.jsx correspond à la route "/" (page d'accueil)
@@ -9,25 +7,9 @@ export const Route = createFileRoute('/') ({
 })
 
 function HomePage() {
-    const [message, setMessage] = useState('No connection yet')
-
-    async function handleTestConnection() {
-        try {
-            const data = await testConnection()
-            setMessage(`Connection successful! The backend says: ${JSON.stringify(data)}`)
-        } catch (error) {
-            setMessage(`Error: ${error.message}`)
-        }
-    }
 
     return (
         <>
-            <button onClick={handleTestConnection}>
-                Tester la connexion au backend
-            </button>
-
-            <p>{message}</p>
-
             <ClothesDisplay />
         </>
     )
