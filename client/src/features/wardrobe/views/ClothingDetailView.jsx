b5bfa754ce9cd983c724c201218290a8650e85e9
IkuroMaira <gwen.bussac@protonmail.com>
2026-04-18 00:40:56 +0200

    - Add field for clothing details - Change Shadcn theme for Lyra

--- client/src/features/wardrobe/views/ClothingDetailView.jsx
+++ client/src/features/wardrobe/views/ClothingDetailView.jsx
@@ -3,7 +3,6 @@ import ClothingForm from '../components/ClothingForm.jsx'
 export default function ClothingDetailView() {
     return (
         <>
-            <h1>Il faut créer le front de cette page</h1>
             <ClothingForm />
         </>
     )
