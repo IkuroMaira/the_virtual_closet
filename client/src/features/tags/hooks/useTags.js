8b4179025040cc4435a1607e8343bffb8105f7d0
Majda-Fg <majda.fougou@gmail.com>
2026-04-17 11:27:30 +0200

    tags display

--- client/src/features/tags/hooks/useTags.js
+++ client/src/features/tags/hooks/useTags.js
@@ -2,17 +2,22 @@
 // Ici toute la logique de gestion des tags
 
 import { useState } from 'react';
+import { getAllTags } from '../../../shared/services/tags_api';
 // import { API_BASE_URL } from '../../../shared/services/api.jsx'
 
-import dataTags from '../mockTags.json'
+/* import dataTags from '../mockTags.json' */
 
 export const useTags = () => {
     // À reprendre plus tard quand on refera la connexion au back avec ORM installé
 
-    const [tags] = useState(dataTags)
+/*     const [tags, setTags] = useState(dataTags) */
+    const [tags, setTags] = useState([])
 
     return {
         tags,
-        fetchTags: () => {}
+        fetchTags: async () => {
+            const data = await getAllTags();
+            setTags(data)
+        }
     };
 };
