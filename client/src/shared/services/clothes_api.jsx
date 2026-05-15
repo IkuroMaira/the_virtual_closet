a6117fcedb1faa3617a3d8af811f5ff103a64ba7
IkuroMaira <gwen.bussac@protonmail.com>
2026-05-15 16:54:58 +0200

    Update useClothes hook to use TanStack Query and remove ClothesContext which wasn't being used anymore

--- client/src/shared/services/clothes_api.jsx
+++ client/src/shared/services/clothes_api.jsx
@@ -6,19 +6,15 @@ import { API_BASE_URL } from "./api.jsx"
  */
 export async function getAllClothes() {
     try {
-        console.log('=> Envoi de la requête vers', API_BASE_URL + '/clothes/')
         const response = await fetch(`${API_BASE_URL}/clothes/`)
 
         if (!response.ok) {
             throw new Error(`Erreur HTTP! Status: ${response.status}`)
         }
 
-        console.log('=> Réponse reçue:', response)
+        const datas = await response.json()
 
-        const data = await response.json()
-        console.log('=> Données:', data)
-
-        return data
+        return datas
 
     } catch (error) {
         console.error('=> Erreur dans getAllClothes:', error)
@@ -39,7 +35,6 @@ export async function getItem(item_id) {
         }
 
         const data = await response.json()
-        console.log('=> Données:', data)
 
         return data
     } catch (error) {
