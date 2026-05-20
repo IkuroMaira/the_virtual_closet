fed58002d37507ab6c5d3a8c1d5f819eba7151e1
Majda-Fg <majda.fougou@gmail.com>
2026-05-20 12:21:08 +0200

    adding TanStackQuery instead of context

--- client/src/features/tags/hooks/useTags.js
+++ client/src/features/tags/hooks/useTags.js
@@ -1,29 +1,20 @@
 // Hooks customisé pour la logique
 // Ici toute la logique de gestion des tags
 
-import { useState, useCallback } from 'react';
 import { getAllTags } from '../../../shared/services/tags_api';
-// import { API_BASE_URL } from '../../../shared/services/api.jsx'
-
-/* import dataTags from '../mockTags.json' */
+import { useQuery } from "@tanstack/react-query"
 
 export const useTags = () => {
-    // À reprendre plus tard quand on refera la connexion au back avec ORM installé
 
-/*     const [tags, setTags] = useState(dataTags) */
-    const [tags, setTags] = useState([])
+    const { isPending, isError, data, error } = useQuery({
+        queryKey: ["tags"],
+        queryFn: () => getAllTags()
+    })
 
-    const fetchTags = useCallback(async () => {
-        const data = await getAllTags();
-        setTags(data)
-    }, []);
-
-    return { tags, fetchTags }
-/*     return {
-        tags,
-        fetchTags: async () => {
-            const data = await getAllTags();
-            setTags(data)
-        }
-    }; */
+    return {
+        isPending,
+        isError,
+        data,
+        error,
+    }
 };
