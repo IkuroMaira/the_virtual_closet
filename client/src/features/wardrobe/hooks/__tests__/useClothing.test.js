9b5f622301f959a7cce65a98f705ee8fc2adb143
IkuroMaira <gwen.bussac@protonmail.com>
2026-05-20 15:14:20 +0200

    WIP

--- client/src/features/wardrobe/hooks/__tests__/useClothing.test.js
+++ client/src/features/wardrobe/hooks/__tests__/useClothing.test.js
@@ -1,8 +1,24 @@
-import { describe, test, expect } from "vitest";
+import { describe, test, expect, vi } from "vitest";
 import { useClothing } from "../useClothing";
+import { renderHook, waitFor} from "@testing-library/react";
+
+const queryClient = new QueryClient();
+const wrapper = ({ children }) => {
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+}
+
+const { result } = renderHook(() => useCustomHook(), { wrapper })
+
+await waitFor(() => expect(result.current.isSuccess).toBe(true))
+
+expect(result.current.data).toEqual('Hello')
 
 describe("useClothing", () => {
     test('should return garment details', () => {
+        const getClothingDetails = vi.fn()
+
+        getClothingDetails.mockReturnValue(result)
+
         expect(useClothing());
     })
 
