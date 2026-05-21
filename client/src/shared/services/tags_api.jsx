e1593e3f2445d2ffc1962139f0e685b5576d9e0d
Gwenaëlle Bussac <94472168+IkuroMaira@users.noreply.github.com>
2026-05-21 15:48:35 +0200

    Feat/add tests and status managment on garment details feat (#210)
    * Update useClothes hook to use TanStack Query and remove ClothesContext which wasn't being used anymore
    
    * Add test librairies and begin test on useClothing
    
    * WIP
    
    * WIP
    
    * WIP
    
    * Add final unit testing on useClothing
    
    * Remove console.log
    
    * Remove try/catch unnecessary
    
    * Remove try/catch on api.jsx

--- client/src/shared/services/tags_api.jsx
+++ client/src/shared/services/tags_api.jsx
@@ -1,19 +1,13 @@
 import { API_BASE_URL } from "./api.jsx"
 
 export async function getAllTags() {
+    const response = await fetch(API_BASE_URL + '/tags/')
 
-    try {
-        const response = await fetch(API_BASE_URL + '/tags/')
-
-        if (!response.ok) {
-            throw new Error(`Erreur HTTP! Status: ${response.status}`) 
-        }
-
-        const data = await response.json()
-
-        return data
-        
-    } catch (error) {
-        throw error
+    if (!response.ok) {
+        throw new Error(`Erreur HTTP! Status: ${response.status}`)
     }
+
+    const data = await response.json()
+
+    return data
 }
