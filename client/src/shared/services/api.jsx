c159b2e84ac62aeec204abdb8127ab79e6784886
IkuroMaira <gwen.bussac@protonmail.com>
2026-05-21 09:57:57 +0200

    Remove console.log

--- client/src/shared/services/api.jsx
+++ client/src/shared/services/api.jsx
@@ -5,25 +5,19 @@ export const API_BASE_URL = '/api'
  * @returns {Promise} - Returns the backend datas
  */
 export async function testConnection() {
-  console.log('=> Appel : testConnection()')
-
   try {
-    console.log('=> Envoi de la requête vers', API_BASE_URL + '/')
     const response = await fetch(API_BASE_URL + '/')
 
     if (!response.ok) {
       throw new Error(`Erreur HTTP! Status: ${response.status}`)
     }
 
-    console.log('=> Réponse reçue:', response)
 
     const data = await response.json()
-    console.log('=> Données:', data)
 
     return data
 
   } catch (error) {
-    console.error('=> Erreur dans testConnection:', error)
     throw error
   }
 }
